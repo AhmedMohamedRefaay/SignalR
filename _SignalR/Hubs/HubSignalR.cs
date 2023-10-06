a0cdbe9f7030efaf6810a8f1b7d363b8166c28ff
TD <TD@DESKTOP-5NGF4PT>
2023-10-06 23:41:34 +0200

    Add project files.

---
+++ _SignalR/Hubs/HubSignalR.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace _SignalR.Hubs
+{
+    public class HubSignalR:Hub
+    {
+        public async Task Send(string FromUser,string Message)
+        {
+            await Clients.All.SendAsync("Recieve", FromUser, Message);
+        }
+
+    }
+}
---
+++ _SignalR/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿//Server
+var signal = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+signal.on("Recieve", function (fromUser, Message) {
+
+    var contect = fromUser + ":" + Message;
+    var li = document.createElement("li");
+    li.textContent = contect;
+    $("#ul").prepend(li);
+
+});
+signal.start();
+
+//Client
+$("#btnsend").on("click",function ()
+{
+    var nm = $("#name").val();
+    var ms = $("#messg").val();
+    signal.invoke("Send" , nm, ms);
+})
